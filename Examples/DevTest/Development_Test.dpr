9636c576ab24b023cd50ab007aff97f7b8a70634
Thimo Braker <thibmorozier@gmail.com>
2016-09-23 02:16:26 +0200

    Cleanup of dev project

--- Examples/DevTest/Development_Test.dpr
+++ Examples/DevTest/Development_Test.dpr
@@ -16,17 +16,17 @@ uses
   GW2DA_Misc in '..\..\src\Misc\GW2DA_Misc.pas',
   GW2DA_Utils in '..\..\src\GW2DA_Utils.pas';
 
+CONST
+  { These keys only exist for development purposes and will be deleted ASAP! }
+  CONST_DEV_API_KEY = '9AA549AC-E2C5-994C-86D0-F49D255886DFB57AE877-A650-474A-8EC0-33FB67B3D12B';
+                      //'C94A01D8-5B6E-A044-B6BA-0040B4E177BEC3F9C17B-FD49-43BB-87EC-282DB2C11493';
+
 var
   fGW2API:        TGW2API;
   fAPIRequestUrl: string;
-  fVersion:       TGW2Version;
   fStringList:    TStringList;
   fTmpString:     string;
   fString:        string;
-  fInteger:       Integer;
-  fColorIDs:      TIntegerArray;
-  fColorList:     TGW2ColorList;
-  fColor:         TGW2Color;
   fParams:        TUrlParams;
 
 begin
@@ -43,15 +43,6 @@ begin
 
     WriteLn;
     WriteLn;
-    fVersion := TGW2Version.Create;
-    fGW2API.Misc.GetBuild(fGW2API.WebHandler, APIv1, fVersion);
-    WriteLn('Build API v1: ' + IntToStr(fVersion.id));
-    fGW2API.Misc.GetBuild(fGW2API.WebHandler, APIv2, fVersion);
-    WriteLn('Build API v2: ' + IntToStr(fVersion.id));
-    FreeAndNil(fVersion);
-
-    WriteLn;
-    WriteLn;
     WriteLn('Quaggans:');
     fStringList := TStringList.Create;
     fGW2API.Misc.GetQuagganIDs(fGW2API.WebHandler, fStringList);
@@ -60,74 +51,6 @@ begin
       WriteLn(fTmpString);
 
     FreeAndNil(fStringList);
-
-    WriteLn;
-    WriteLn;
-    WriteLn('All Color IDs: ');
-    fColorIDs := fGW2API.Misc.GetColorIDs(fGW2API.WebHandler);
-    fTmpString := '';
-
-    for fInteger in fColorIDs do
-      if fTmpString = '' then
-        fTmpString := IntToStr(fInteger)
-      else
-        fTmpString := fTmpString + ', ' + IntToStr(fInteger);
-
-    WriteLn(fTmpString);
-
-    WriteLn;
-    WriteLn;
-    WriteLn('Colors:');
-    SetLength(fParams, 1);
-    fParams[0].Name  := 'ids';
-    fParams[0].Value := '1,2,3';
-    fColorList := fGW2API.Misc.GetColors(fGW2API.WebHandler, fParams);
-
-    for fColor in fColorList do
-    begin
-      WriteLn('ID: ' + IntToStr(fColor.id));
-      WriteLn('Name: ' + fColor.Name);
-      WriteLn(Format('Base_RGB: %d,%d,%d', [fColor.Base_RGB[0],
-                                            fColor.Base_RGB[1],
-                                            fColor.Base_RGB[2]]));
-      WriteLn('Cloth: ');
-      WriteLn(#9 + 'Brightness' + IntToStr(fColor.Cloth.Brightness));
-      WriteLn(#9 + Format('Contrast: %n', [fColor.Cloth.Contrast]));
-      WriteLn(#9 + 'Hue' + IntToStr(fColor.Cloth.Hue));
-      WriteLn(#9 + Format('Saturation: %n', [fColor.Cloth.Saturation]));
-      WriteLn(#9 + Format('Lightness: %n', [fColor.Cloth.Lightness]));
-      WriteLn(#9 + Format('RGB: %d,%d,%d', [fColor.Cloth.RGB[0],
-                                            fColor.Cloth.RGB[1],
-                                            fColor.Cloth.RGB[2]]));
-      WriteLn('Leather: ');
-      WriteLn(#9 + 'Brightness' + IntToStr(fColor.Leather.Brightness));
-      WriteLn(#9 + Format('Contrast: %n', [fColor.Leather.Contrast]));
-      WriteLn(#9 + 'Hue' + IntToStr(fColor.Leather.Hue));
-      WriteLn(#9 + Format('Saturation: %n', [fColor.Leather.Saturation]));
-      WriteLn(#9 + Format('Lightness: %n', [fColor.Leather.Lightness]));
-      WriteLn(#9 + Format('RGB: %d,%d,%d', [fColor.Leather.RGB[0],
-                                            fColor.Leather.RGB[1],
-                                            fColor.Leather.RGB[2]]));
-      WriteLn('Metal: ');
-      WriteLn(#9 + 'Brightness' + IntToStr(fColor.Metal.Brightness));
-      WriteLn(#9 + Format('Contrast: %n', [fColor.Metal.Contrast]));
-      WriteLn(#9 + 'Hue' + IntToStr(fColor.Metal.Hue));
-      WriteLn(#9 + Format('Saturation: %n', [fColor.Metal.Saturation]));
-      WriteLn(#9 + Format('Lightness: %n', [fColor.Metal.Lightness]));
-      WriteLn(#9 + Format('RGB: %d,%d,%d', [fColor.Metal.RGB[0],
-                                            fColor.Metal.RGB[1],
-                                            fColor.Metal.RGB[2]]));
-      WriteLn('Item: ' + IntToStr(fColor.Item));
-      fTmpString := '';
-
-      for fString in fColor.Categories do
-        if fTmpString = '' then
-          fTmpString := fString
-        else
-          fTmpString := fTmpString + ', ' + fString;
-
-      WriteLn('Categories: ' + fTmpString);
-    end;
 
     FreeAndNil(fGW2API);
 
