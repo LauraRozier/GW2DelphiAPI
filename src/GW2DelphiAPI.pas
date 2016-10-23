accf8e5471d46a5d9a25ac318e6a69d02e69f634
Thimo Braker <thibmorozier@live.nl>
2016-10-23 21:20:10 +0200

    Adds the account outfits function
    Signed-off-by: Thimo Braker <thibmorozier@live.nl>

--- src/GW2DelphiAPI.pas
+++ src/GW2DelphiAPI.pas
@@ -687,6 +687,41 @@ begin
 end;
 
 
+//* Version: 51
+//* Class: Account
+//* aWebHandler: The API webhandler object
+//* aState: The API state object
+//* Result: Returns an array mini IDs
+function TGW2APIAccount.GetOutfits(aWebHandler: TWebHandler; aState: TStateHoler): TIntegerArray;
+var
+  Utils:    TGW2Helper;
+  Reply:    string;
+  JSArr:    TJSONArray;
+  I:        Integer;
+begin
+  Utils := TGW2Helper.Create;
+
+  if aState.AuthString <> '' then
+    if Utils.ArrContains(aState.AuthToken.Permissions, 'unlocks') then
+    begin
+      Reply := aWebHandler.FetchAuthEndpoint(APIv2, v2AccountOutfits, nil, aState.AuthString);
+      JSArr := TJSONObject.ParseJSONValue(Reply) as TJSONArray;
+      SetLength(Result, JSArr.Count);
+
+      for I := 0 to JSArr.Count - 1 do
+      begin
+        if JSArr.Items[I].Null then
+          Continue;
+
+        Result[I] := StrToInt(JSArr.Items[I].Value);
+      end;
+    end else
+      raise Exception.Create('Error: The provided API key does not have enough permissions!')
+  else
+    raise Exception.Create('Error: No valid API key has been entered!');
+end;
+
+
 { API Misc functions class }
 //* Version: 9
 //* Class: Misc
